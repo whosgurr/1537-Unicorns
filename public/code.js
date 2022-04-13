99ccf2145245cabce5a1e71869c3de041a88e998
Yuen Mo <garymo@hotmail.ca>
2022-04-13 10:56:15 -0700

    table

--- public/code.js
+++ public/code.js
@@ -5,28 +5,6 @@ function process_res(data){
     SAVED_DATA = data
     result = ""
 
-<div class="container">
-  <table id="table" data-toggle="table" data-flat="true">
-    <thead>
-      <tr>
-        <th data-field="unicorns._id" data-sortable="true">ID</th>
-        <th data-field="unicorns.name" data-sortable="true">Name</th>
-        <th data-field="unicorns.dob" data-sortable="true">Date of Birth</th>
-        <th data-field="unicorns.loves" data-sortable="true">Loves</th>
-        <th data-field="unicorns.weight" data-sortable="true"><small>Weight</small><br/>Power Cash</th>
-        <th data-field="unicorns.gender" data-sortable="true"><small>Gender</small><br/>Blocked Cash</th>
-        <th data-field="unicorns.vampires" data-sortable="true"><small>Vampires</small><br/>Credit Cash</th>
-      </tr>
-    </thead>
-  </table>
-</div>
-
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-<script src="https://unpkg.com/bootstrap-table@1.18.0/dist/bootstrap-table.min.js"></script>
-    
-
 
     // for (i = 0; i < SAVED_DATA.length; i++) {
     //     // for each unicorn
